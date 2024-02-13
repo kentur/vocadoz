cff1b52a24815389eda33a58a003c799e57cb184
kentur <159957858+kentur@users.noreply.github.com>
2024-02-13 15:54:04 -0500

    Update i18n.js

--- src/language/i18n.js
+++ src/language/i18n.js
@@ -1,15 +1,11 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import { english } from './en'
 import { spanish } from './es'
 
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
-    en: {
-        translation: english,
-    },
     es: {
         translation: spanish,
     },
